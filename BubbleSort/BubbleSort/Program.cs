7012131134a0b08d60f83b7f806e9c4094f989af
beerappa <beerappadandaragi@gmail.com>
2021-11-24 13:57:33 +0530

    Welcome To Algorithm Programs

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 58, 45, 66, 55, 13 };
+            int temp;
+
+            for (int j = 0; j < arr.Length - 1; j++)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("sorted");
+            foreach (int b in arr)
+            {
+                Console.WriteLine(b + "");
+
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
