be398c93be17fc1d5c6607e98ef663612d547fdf
beerappa <beerappadandaragi@gmail.com>
2021-11-24 11:10:00 +0530

    Welcome To Algorithm Programs

---
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wel Come To Algorithm Programs");
+            Console.ReadLine();
+        }
+        
+    }
+}
